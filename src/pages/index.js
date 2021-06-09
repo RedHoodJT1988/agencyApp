faa40186fe8e49c54244f642f0b45f200f4949dd
ProgrammingWithJon <programmingwithjon@gmail.com>
2021-06-09 15:33:09 -0500

    add second section

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,23 +1,72 @@
 import React, { Component } from "react"
 import { Link } from "gatsby"
-import { Banner, TextWrapper, MoreText } from "../styles/IndexStyles"
+import {
+  Banner,
+  TextWrapper,
+  MoreText,
+  SectionTwo,
+} from "../styles/IndexStyles"
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome"
+import { library } from "@fortawesome/fontawesome-svg-core"
+import { faHeart, faCode, faGem, fas } from "@fortawesome/free-solid-svg-icons"
+
+library.add(faHeart, faCode, faGem, fas)
 
 export default () => (
-  <div style={{ position: "relative" }}>
-    <Banner></Banner>
-    <TextWrapper>
+  <>
+    <section style={{ position: "relative" }}>
+      <Banner></Banner>
+      <TextWrapper>
+        <div>
+          <h2>Goofy Programmer</h2>
+          <p>
+            One Stop Shop for
+            <br />
+            All of your development
+            <br />
+            And design needs
+          </p>
+          <Link to="/works">Our Works</Link>
+        </div>
+      </TextWrapper>
+      <MoreText>Learn More</MoreText>
+    </section>
+    <SectionTwo>
       <div>
-        <h2>Goofy Programmer</h2>
+        <h2>Our Passion</h2>
         <p>
-          One Stop Shop for
+          Most good programmers do programming not because they expect to get
+          paid,
           <br />
-          All of your development
-          <br />
-          And design needs
+          but because it's fun to program.
         </p>
-        <Link to="/works">Our Works</Link>
+        <h5> - Linus Torvalds</h5>
+        <span>
+          <FontAwesomeIcon
+            icon="gem"
+            color="#04F5C6"
+            size="6x"
+            style={{ marginRight: "3rem" }}
+            fixedWidth
+            border
+          />
+          <FontAwesomeIcon
+            icon="heart"
+            color="#00F0FF"
+            size="6x"
+            style={{ marginRight: "3rem" }}
+            fixedWidth
+            border
+          />
+          <FontAwesomeIcon
+            icon="code"
+            color="#73DBFD"
+            size="6x"
+            fixedWidth
+            border
+          />
+        </span>
       </div>
-    </TextWrapper>
-    <MoreText>Learn More</MoreText>
-  </div>
+    </SectionTwo>
+  </>
 )
--- src/styles/IndexStyles.js
+++ src/styles/IndexStyles.js
@@ -56,4 +56,41 @@ const MoreText = styled.div`
   }
 `
 
-export { Banner, TextWrapper, MoreText }
+const SectionTwo = styled.section`
+  background-color: #21b2a6;
+  text-align: center;
+  padding: 10rem 0;
+  div {
+    width: 66%;
+    margin: 0 auto;
+  }
+  h2 {
+    font-size: 3rem;
+    padding: 1.35em 0;
+    color: #ffffff;
+    border-bottom: 2px solid #1d9c91;
+    text-transform: uppercase;
+    letter-spacing: 0.6rem;
+    margin: 0;
+  }
+  p {
+    text-transform: uppercase;
+    color: #c8ece9;
+    text-align: center;
+    letter-spacing: 0.225em;
+    font-size: 1.5rem;
+  }
+  h5 {
+    font-size: 1.4rem;
+    line-height: 2rem;
+    color: #ffffff;
+    border-bottom: 2px solid #1d9c91;
+    font-weight: 800;
+    letter-spacing: 0.225em;
+    text-transform: uppercase;
+    padding-bottom: 0.5rem;
+    margin-bottom: 5rem;
+  }
+`
+
+export { Banner, TextWrapper, MoreText, SectionTwo }
