d9cee6076f8e2a155a03af7d2a64176a7cef0c8e
ProgrammingWithJon <programmingwithjon@gmail.com>
2021-06-09 12:17:36 -0500

    finish homepage and add all images

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,5 +1,23 @@
-import React from "react"
+import React, { Component } from "react"
+import { Link } from "gatsby"
+import { Banner, TextWrapper, MoreText } from "../styles/IndexStyles"
 
-export default function Home() {
-  return <div>Hello world!</div>
-}
+export default () => (
+  <div style={{ position: "relative" }}>
+    <Banner></Banner>
+    <TextWrapper>
+      <div>
+        <h2>Goofy Programmer</h2>
+        <p>
+          One Stop Shop for
+          <br />
+          All of your development
+          <br />
+          And design needs
+        </p>
+        <Link to="/works">Our Works</Link>
+      </div>
+    </TextWrapper>
+    <MoreText>Learn More</MoreText>
+  </div>
+)
